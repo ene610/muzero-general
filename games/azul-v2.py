59c084167786d7c928dfaecc740961b4eb8d2129
ene610 <31830741+ene610@users.noreply.github.com>
2021-03-08 12:46:22 +0100

    Update azul-v2.py

--- games/azul-v2.py
+++ games/azul-v2.py
@@ -33,7 +33,10 @@ class MuZeroConfig:
         ### Self-Play
         self.num_workers = 1  # Number of simultaneous threads/workers self-playing to feed the replay buffer
         self.selfplay_on_gpu = False
-        self.max_moves = 2000  # Maximum number of moves if game is not finished before
+
+        #n_min azioni = 9 n_max = 24
+
+        self.max_moves = 25  # Maximum number of moves if game is not finished before
         self.num_simulations = 25  # Number of future moves self-simulated
         self.discount = 1  # Chronological discount of the reward
         self.temperature_threshold = None  # Number of moves before dropping the temperature given by visit_softmax_temperature_fn to 0 (ie selecting the best action). If None, visit_softmax_temperature_fn is used every time
